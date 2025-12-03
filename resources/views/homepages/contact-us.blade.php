c160c34b344d6dcdaf48c100eb3d03d437fa6112
chiefJurist <anthonynnanna004@gmail.com>
2025-12-03 13:37:26 +0100

    contact us

--- resources/views/homepages/contact-us.blade.php
+++ resources/views/homepages/contact-us.blade.php
@@ -3,7 +3,7 @@
         <div class="general-div-container bg-white font-bold basis-1/2 text-center">
             <div class="text-3xl md:text-4xl py-5">HOW CAN WE HELP?</div>
             <div class="text-xl pt-24">SEND A MESSAGE TO?</div>
-            <div class="text-xl py-3">SUPPORT@TRUVOTRADE.COM</div>
+            <div class="text-xl py-3">SUPPORT@PRIMECOPIER.COM</div>
         </div>
 
         <div class="general-div-container font-bold basis-1/2">
@@ -11,16 +11,16 @@
             <div class="pt-24 font-nunito text-sm md:text-lg">
                 <span class="text-gray-500">General Enquiries: </span>
                 <span class="text-blue-500">
-                    <a href="mailto:support@truvotrade.com"> [support@truvotrade.com]</a>
+                    <a href="mailto:support@primecopier.com"> [support@primecopier.com]</a>
                 </span>
             </div>
             <div class="py-5 font-nunito text-sm md:text-lg">
                 <span class="text-gray-500">Support: </span>
                 <span class="text-blue-500">
-                    <a href="mailto:support@truvotrade.com"> [support@truvotrades.com]</a>
+                    <a href="mailto:support@primecopier.com"> [support@primecopier.com]</a>
                 </span>
             </div>
-            <div class="text-gray-500">Truvotrade Address:</div>
+            <div class="text-gray-500">Primecopier Address:</div>
             <div class="text-gray-400 font-medium">1910 Pacific Ave, Dallas, TX 75201</div>
         </div>
     </div>
