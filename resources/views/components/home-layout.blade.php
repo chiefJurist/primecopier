06dd456f5b43f6f0b4ea608bbc85db39e955207f
chiefJurist <anthonynnanna004@gmail.com>
2025-12-03 16:19:22 +0100

    tawk to

--- resources/views/components/home-layout.blade.php
+++ resources/views/components/home-layout.blade.php
@@ -52,7 +52,7 @@
         (function(){
         var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
         s1.async=true;
-        s1.src='https://embed.tawk.to/68ca82adb1960d1924ccd0f3/1j5bgsu0b';
+        s1.src='https://embed.tawk.to/69305458560c91197f37b752/1jbiciunt';
         s1.charset='UTF-8';
         s1.setAttribute('crossorigin','*');
         s0.parentNode.insertBefore(s1,s0);
